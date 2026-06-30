8d28e7d5aee9b648197f3b8bb9b3f40cee384271
Adi Affan <adiaffan12@gmail.com>
2026-06-30 12:33:11 +0530

    Structure Spacing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+     root: new URL('./src', import.meta.url).pathname,
 })
