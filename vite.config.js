db317ff0ad5142cd25f730f4b2675b3621b605dd
Adi Affan <adiaffan12@gmail.com>
2026-06-30 13:00:35 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-     root: new URL('./src', import.meta.url).pathname,
-     base: '/Clothing-Store-Vanilla-JS2.0/'
+  root: new URL('./src', import.meta.url).pathname,
+  base: '/Clothing-Store-Vanilla-JS2.0/',
+  build: {
+    outDir: '../dist', // Changes the output folder from 'dist' to 'build'
+  },
 })
